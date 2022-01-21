2655b327d5fa9f3ac14b1931d1d79e6c1e5e8ca9
BrayanCordero <corderobrayan@yahoo.com>
2022-01-20 16:49:41 -0800

    sql document

--- Tests/ModelsTest.cs
+++ Tests/ModelsTest.cs
@@ -40,8 +40,57 @@ public class UnitTest1
     }
 
 
-    
+    [Fact]
+    public void OrderShouldSetData()
+    {
+        Order testorder = new Order();
+        string OrderDate = "01/01/1990";
+        string Customer ="testCustomer";
+        int OrderNumber = 999;
+        string StoreName ="Munchies";
+        decimal Total = 12.50M;
 
+        testorder.OrderDate = OrderDate;
+        testorder.Customer = Customer;
+        testorder.OrderNumber = OrderNumber;
+        testorder.StoreName = StoreName;
+        testorder.Total = Total;
+
+        Assert.Equal(OrderDate, testorder.OrderDate);
+        Assert.Equal(Customer, testorder.Customer);
+        Assert.Equal(OrderNumber, testorder.OrderNumber);
+        Assert.Equal(StoreName, testorder.StoreName);
+        Assert.Equal(Total, testorder.Total);
+
+
+    }
+
+    [Fact]
+    public void CustomerShouldSetData()
+    {
+        Customer testCustomer = new Customer();
+
+        string UserName = "testUsername";
+        string Password = "1234";
+        string Email = "test@gmail.com";
+
+        testCustomer.UserName = UserName;
+        testCustomer.Password = Password;
+        testCustomer.Email = Email;
+
+        Assert.Equal(UserName, testCustomer.UserName);
+        Assert.Equal(Password, testCustomer.Password);
+        Assert.Equal(Email, testCustomer.Email);
+
+    }
+
+    [Fact]
+    public void ProductShouldBeAddedToCart()
+    {
+        List<LineItem> testCart = new List<LineItem>();
+        LineItem testlineitem = new LineItem();
+
+    }
 
 
 
